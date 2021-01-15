4142880a1435405728bfdf68475635db5c604508
Samuel Lee <samuelxlee@gmail.com>
2021-01-14 19:52:22 -0800

    wrote middleware for validating action id and that the response body for actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const Actions = require("./actions-model");
 
 router.get("/", (req, res, next) => {
-    Actions.get(req.id)
+    Actions.get(req.params.id)
         .then(actions => {
             res.status(200).json(actions);
         })
@@ -13,11 +13,15 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
-    Actions.get(req.id)
+    Actions.get(req.params.id)
         .then(action => {
             res.status(200).json(action);
         })
         .catch(next);
 });
+
+router.post("/", (req, res, next) => {
+    Actions.insert(req.body)
+})
 
 module.exports = router;
---
+++ api/middleware/middleware.js
@@ -0,0 +1,29 @@
+const Actions = require("../actions/actions-model");
+const Projects = require("../projects/projects-model");
+
+async function validateActionId(req, res, next) {
+    try {
+        const action = await Actions.get(req.params.id);
+        if (action) {
+            req.action = action;
+            next();
+        } else {
+            res.status(404).json({ message: "action with that id not found" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "error retreiving action data" });
+    }
+}
+
+function validateAction(req, res, next) {
+    const projectId = req.body.projectId;
+    if (!req.body) {
+        res.status(400).json({ message: "missing action data" });
+    } else if (!req.body.projectId || !req.body.description || !req.body.notes) {
+        res.status(400).json({ message: "project id, description, and notes required" });
+    } else if (projectId !== Projects.get(projectId)) {
+        res.status(404).json({ message: "project with that id not found" });
+    } else if (!req.body.description.length > 128) {
+        res.status(400).json({ message: "description length too long (max 128 chars.)" });
+    }
+}
