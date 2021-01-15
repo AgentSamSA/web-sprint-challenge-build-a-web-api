96e8b969baeef590bc3f8da6d1ccdc6a0c7609c0
Samuel Lee <samuelxlee@gmail.com>
2021-01-14 20:11:28 -0800

    wrote endpoints in the actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 const Actions = require("./actions-model");
 
+const { validateActionId, validateAction } = require("../middleware/middleware");
+
 router.get("/", (req, res, next) => {
     Actions.get(req.params.id)
         .then(actions => {
@@ -12,16 +14,40 @@ router.get("/", (req, res, next) => {
         .catch(next);
 });
 
-router.get("/:id", (req, res, next) => {
-    Actions.get(req.params.id)
+router.get("/:id", validateActionId, (req, res) => {
+    res.status(200).json(req.action);
+});
+
+router.post("/", validateAction, (req, res, next) => {
+    Actions.insert(req.body)
+        .then(action => {
+            res.status(201).json(action);
+        })
+        .catch(next);
+});
+
+router.put("/:id", validateActionId, validateAction, (req, res, next) => {
+    Actions.update(req.params.id, req.body)
         .then(action => {
             res.status(200).json(action);
         })
         .catch(next);
 });
 
-router.post("/", (req, res, next) => {
-    Actions.insert(req.body)
-})
+router.delete("/:id", validateActionId, (req, res, next) => {
+    Actions.remove(req.params.id)
+        .then(() => {
+            res.status(200).json({ message: "the action has been deleted" });
+        })
+        .catch(next);
+});
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        info: "something went wrong inside the actions router",
+        message: err.message,
+        stack: err.stack
+    });
+});
 
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,2 +1,53 @@
-// Write your "projects" router here!
+const express = require("express");
+
+const router = express.Router();
+
+const Projects = require("./projects-model");
+
+const { validateProjectId, validateProject } = require("../middleware/middleware");
+
+router.get("/", (req, res, next) => {
+    Projects.get(req.params.id)
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(next);
+});
+
+router.get("/:id", validateProjectId, (req, res) => {
+    res.status(200).json(req.action);
+});
+
+router.post("/", validateProject, (req, res, next) => {
+    Projects.insert(req.body)
+        .then(action => {
+            res.status(201).json(action);
+        })
+        .catch(next);
+});
+
+router.put("/:id", validateProjectId, validateProject, (req, res, next) => {
+    Projects.update(req.params.id, req.body)
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(next);
+});
+
+router.delete("/:id", validateProjectId, (req, res, next) => {
+    Projects.remove(req.params.id)
+        .then(() => {
+            res.status(200).json({ message: "the project has been deleted" });
+        })
+        .catch(next);
+});
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        info: "something went wrong inside the projects router",
+        message: err.message,
+        stack: err.stack
+    });
+});
+
 module.exports = router;
